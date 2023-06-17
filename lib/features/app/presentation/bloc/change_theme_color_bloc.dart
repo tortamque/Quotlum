9820ffe30cbd05ef577b88d203250e5f540eff14
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-17 18:57:19 +0300

    Create bloc for change theme color

---
+++ lib/features/app/presentation/bloc/change_theme_color_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quotlum/config/theme/colors.dart';
+
+class ChangeThemeColorBloc extends Bloc<ChangeThemeColorEvent, Color>{
+  ChangeThemeColorBloc() : super(CustomColors.mainColor){
+    on<ChangeThemeColorEvent>(_onChangeThemeColor);
+  }
+
+  _onChangeThemeColor(ChangeThemeColorEvent event, Emitter<Color> emit){
+    emit(event.newColor);
+  }
+}
+
+class ChangeThemeColorEvent {
+  final Color newColor;
+
+  ChangeThemeColorEvent(this.newColor);
+}
