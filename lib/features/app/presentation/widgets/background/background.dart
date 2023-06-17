bda4ec5cf8bcfe17b4862f79bf606ad06d6d3fa2
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-17 19:20:54 +0300

    Implement background color change on event

--- lib/features/app/presentation/widgets/background/background.dart
+++ lib/features/app/presentation/widgets/background/background.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quotlum/config/theme/colors.dart';
+import 'package:quotlum/features/app/presentation/bloc/change_theme_color_bloc.dart';
 
 class Background extends StatelessWidget {
   const Background({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: CustomColors.mainColor
+    return BlocBuilder<ChangeThemeColorBloc, Color>(
+      builder: (context, state) {
+        return Container(
+          color: CustomColors.mainColor
+        );
+      },
     );
   }
 }
