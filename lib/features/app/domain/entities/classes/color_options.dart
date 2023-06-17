ca1f5973687aa347adedabc78d2ac4284107e7ae
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-17 20:14:16 +0300

    Add more color options

--- lib/features/app/domain/entities/classes/color_options.dart
+++ lib/features/app/domain/entities/classes/color_options.dart
@@ -3,8 +3,24 @@ import 'package:quotlum/features/app/presentation/widgets/color_change/color_opt
 
 class ColorOptions{
   static const List<ColorOption> colors = [
-    ColorOption(color: Colors.greenAccent),
-    ColorOption(color: Colors.blueAccent),
-    ColorOption(color: Colors.amber),
+    ColorOption(color:Colors.redAccent),
+    ColorOption(color:Colors.blueAccent),
+    ColorOption(color:Colors.cyanAccent),
+    ColorOption(color:Colors.limeAccent),
+    ColorOption(color:Colors.pinkAccent),
+    ColorOption(color:Colors.tealAccent),
+    ColorOption(color:Colors.greenAccent),
+    ColorOption(color:Colors.amberAccent),
+    ColorOption(color:Colors.indigoAccent),
+    ColorOption(color:Colors.orangeAccent),
+    ColorOption(color:Colors.purpleAccent),
+    ColorOption(color:Colors.yellowAccent),
+    ColorOption(color:Colors.lightBlueAccent),
+    ColorOption(color:Colors.deepOrangeAccent),
+    ColorOption(color:Colors.deepPurpleAccent),
+    ColorOption(color:Colors.lightGreenAccent),
+    ColorOption(color:Colors.black),
+    ColorOption(color:Colors.grey),
+    ColorOption(color:Colors.white)
   ];
 }
