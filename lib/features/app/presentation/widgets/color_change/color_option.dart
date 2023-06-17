7d857ed2316c6059415d8f030d9f9477f3e37fae
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-17 20:42:45 +0300

    Add more field to color option

--- lib/features/app/presentation/widgets/color_change/color_option.dart
+++ lib/features/app/presentation/widgets/color_change/color_option.dart
@@ -3,10 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quotlum/features/app/presentation/bloc/change_theme_color_bloc.dart';
 
 class ColorOption extends StatelessWidget {
-  final Color color;
+  final Color mainColor;
+  final Color secondaryColor;
+  final Color tertiaryColor;
 
   const ColorOption({
-    required this.color,
+    required this.mainColor,
+    required this.secondaryColor,
+    required this.tertiaryColor,
     super.key
   });
 
@@ -17,12 +21,16 @@ class ColorOption extends StatelessWidget {
       child: GestureDetector(
         onTap: (){
           BlocProvider.of<ChangeThemeColorBloc>(context).add(
-            ChangeThemeColorEvent(color)
+            ChangeThemeColorEvent(
+              mainColor: mainColor,
+              secondaryColor: secondaryColor,
+              tertiaryColor: tertiaryColor
+            )
           );
         },
         child: Container(
           decoration: BoxDecoration(
-            color: color,
+            color: mainColor,
             borderRadius: const BorderRadius.all(Radius.circular(20)),
             boxShadow: const [
               BoxShadow(
