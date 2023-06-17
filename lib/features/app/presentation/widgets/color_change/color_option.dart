3ed6b691bc64f5c0ddc533d9fbb5953ad4ff3284
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-18 00:01:17 +0300

    Add rotation animation to ColorOption widget

--- lib/features/app/presentation/widgets/color_change/color_option.dart
+++ lib/features/app/presentation/widgets/color_change/color_option.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quotlum/features/app/presentation/bloc/change_theme_color_bloc.dart';
 
-class ColorOption extends StatelessWidget {
+class ColorOption extends StatefulWidget {
   final Color mainColor;
   final Color secondaryColor;
   final Color tertiaryColor;
+
 
   const ColorOption({
     required this.mainColor,
@@ -15,31 +16,64 @@ class ColorOption extends StatelessWidget {
   });
 
   @override
+  State<ColorOption> createState() => _ColorOptionState();
+}
+
+class _ColorOptionState extends State<ColorOption> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutCubic, 
+    );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(30),
-      child: GestureDetector(
-        onTap: (){
-          BlocProvider.of<ChangeThemeColorBloc>(context).add(
-            ChangeThemeColorEvent(
-              mainColor: mainColor,
-              secondaryColor: secondaryColor,
-              tertiaryColor: tertiaryColor
-            )
-          );
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            color: mainColor,
-            borderRadius: const BorderRadius.all(Radius.circular(20)),
-            boxShadow: const [
-              BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.2),
-                offset: Offset(4, 4),
-                blurRadius: 5,
-                spreadRadius: 0,
+      child: RotationTransition(
+        turns: _animation,
+        child: GestureDetector(
+          onTap: (){
+            _controller.reset();
+            _controller.forward();
+            BlocProvider.of<ChangeThemeColorBloc>(context).add(
+              ChangeThemeColorEvent(
+                mainColor: widget.mainColor,
+                secondaryColor: widget.secondaryColor,
+                tertiaryColor: widget.tertiaryColor
               )
-            ]
+            );
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: widget.mainColor,
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.2),
+                  offset: Offset(4, 4),
+                  blurRadius: 5,
+                  spreadRadius: 0,
+                )
+              ]
+            ),
           ),
         ),
       )
