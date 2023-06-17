1d82da2b4832c5e964b4c631bcb80a2162c06a91
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-17 20:43:06 +0300

    Rework ColorOptions class

--- lib/features/app/domain/entities/classes/color_options.dart
+++ lib/features/app/domain/entities/classes/color_options.dart
@@ -3,24 +3,24 @@ import 'package:quotlum/features/app/presentation/widgets/color_change/color_opt
 
 class ColorOptions{
   static const List<ColorOption> colors = [
-    ColorOption(color:Colors.redAccent),
-    ColorOption(color:Colors.blueAccent),
-    ColorOption(color:Colors.cyanAccent),
-    ColorOption(color:Colors.limeAccent),
-    ColorOption(color:Colors.pinkAccent),
-    ColorOption(color:Colors.tealAccent),
-    ColorOption(color:Colors.greenAccent),
-    ColorOption(color:Colors.amberAccent),
-    ColorOption(color:Colors.indigoAccent),
-    ColorOption(color:Colors.orangeAccent),
-    ColorOption(color:Colors.purpleAccent),
-    ColorOption(color:Colors.yellowAccent),
-    ColorOption(color:Colors.lightBlueAccent),
-    ColorOption(color:Colors.deepOrangeAccent),
-    ColorOption(color:Colors.deepPurpleAccent),
-    ColorOption(color:Colors.lightGreenAccent),
-    ColorOption(color:Colors.black),
-    ColorOption(color:Colors.grey),
-    ColorOption(color:Colors.white)
+    ColorOption(mainColor:Colors.redAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.blueAccent, secondaryColor: Colors.black, tertiaryColor: Colors.white),
+    ColorOption(mainColor:Colors.cyanAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.limeAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.pinkAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.tealAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.greenAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.amberAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.indigoAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.orangeAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.purpleAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.yellowAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.lightBlueAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.deepOrangeAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.deepPurpleAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.lightGreenAccent, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.black, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.grey, secondaryColor: Colors.white, tertiaryColor: Colors.black),
+    ColorOption(mainColor:Colors.white, secondaryColor: Colors.white, tertiaryColor: Colors.black)
   ];
 }
