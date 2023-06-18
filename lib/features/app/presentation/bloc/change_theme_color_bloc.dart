207a8c631c78f8e1f55c5bb4d226de0174169347
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-18 11:09:59 +0300

    Rework bloc logic

--- lib/features/app/presentation/bloc/change_theme_color_bloc.dart
+++ lib/features/app/presentation/bloc/change_theme_color_bloc.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quotlum/config/theme/colors.dart';
 
-class ChangeThemeColorBloc extends Bloc<ChangeThemeColorEvent, Color>{
-  ChangeThemeColorBloc() : super(CustomColors.appBackgroundColor){
+class ChangeThemeColorBloc extends Bloc<ChangeThemeColorEvent, ColorState>{
+  ChangeThemeColorBloc() : super(
+    ColorState(previous: CustomColors.appbarContentColor, current: CustomColors.appbarContentColor)
+  ){
     on<ChangeThemeColorEvent>(_onChangeThemeColor);
   }
 
-  _onChangeThemeColor(ChangeThemeColorEvent event, Emitter<Color> emit){
+  _onChangeThemeColor(ChangeThemeColorEvent event, Emitter<ColorState> emit){
+    Color previous = CustomColors.appbarContentColor;
+    Color current = event.appbarContentColor;
+
     CustomColors.appBackgroundColor = event.appBackgroundColor;
     CustomColors.appbarContentColor = event.appbarContentColor;
 
-    emit(event.appBackgroundColor);
+    emit(ColorState(previous: previous, current: current));
   }
 }
 
@@ -22,5 +27,15 @@ class ChangeThemeColorEvent {
   ChangeThemeColorEvent({
     required this.appBackgroundColor,
     required this.appbarContentColor,
+  });
+}
+
+class ColorState{
+  final Color previous;
+  final Color current;
+
+  const ColorState({
+    required this.previous,
+    required this.current
   });
 }
