bcd7d2927fd8dfa9f758b21696af889e02b637bd
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-19 12:39:13 +0300

    Create GetThemeColor bloc

---
+++ lib/features/app/presentation/bloc/get_theme_color_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quotlum/config/theme/colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class GetThemeColorBloc extends Bloc<GetThemeColorEvent, void>{
+  GetThemeColorBloc() : super(null){
+    on<GetThemeColorEvent>(_onGetThemeColor);
+  }
+
+  _onGetThemeColor(GetThemeColorEvent event, Emitter<void> emit) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final int appBackgroundColor = prefs.getInt('appBackgroundColor') ?? CustomColors.appBackgroundColor.value;
+    final int appbarContentColor = prefs.getInt('appbarContentColor') ?? CustomColors.appbarContentColor.value;
+
+    CustomColors.appBackgroundColor = Color(appBackgroundColor);
+    CustomColors.appbarContentColor = Color(appbarContentColor);
+
+    emit(null);
+  }
+}
+
+class GetThemeColorEvent{}
