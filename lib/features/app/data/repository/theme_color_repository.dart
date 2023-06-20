e69fe0ca34f87db1c95d83c6657343c0ac4b601c
tortamque <90132962+tortamque@users.noreply.github.com>
2023-06-20 19:40:59 +0300

    Create repository for theme color retrieval

---
+++ lib/features/app/data/repository/theme_color_repository.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:quotlum/features/app/data/data_sources/local/theme_color_data_source.dart';
+
+class ThemeColorRepository {
+  static Future<Color> getAppBackgroundColor() async {
+    final int colorValue = await ThemeColorDataSource.getAppBackgroundColor();
+    return Color(colorValue);
+  }
+
+  static Future<Color> getAppbarContentColor() async {
+    final int colorValue = await ThemeColorDataSource.getAppbarContentColor();
+    return Color(colorValue);
+  }
+}
